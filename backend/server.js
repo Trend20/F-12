2adf88a08a547217c115b61d7c412eed0ebcd891
DevEnock <enockomondi305@gmail.com>
2022-03-12 23:59:44 +0300

    adding connection string

--- backend/server.js
+++ backend/server.js
@@ -2,21 +2,26 @@ const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose');
 
-const app = express();
-
 const userRoute = require('./routes/User');
 
-require('dotenv').config();
 
+const app = express(); 
+
+
+require('dotenv').config();
 const url = process.env.DATABASE_URL;
 mongoose.connect(url)
 const connection = mongoose.connection;
+
 connection.once('open', () =>{
   console.log('Application connected to the DB!!');
 })
 
 app.use(cors());
+app.use(express.json());
 app.use('/user', userRoute);
+
+
 
 const PORT = process.env.PORT || 8090;
 
