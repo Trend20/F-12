949144c5dfd235bdd7a6ee6af986f11cce3e596e
DevEnock <enockomondi305@gmail.com>
2022-03-14 13:37:53 +0300

    adding the service component

---
+++ src/components/Services/Services.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import './Services.css';
+
+function Services() {
+  return (
+    <div className='services'>
+      <div className="section-header">
+        <h3>What we provide</h3>
+        <h1>IT solutions</h1>
+      </div>
+
+      <div className="services-container">
+
+        <div className="service">
+          <i class="flaticon-project-management"></i>
+          <h3>IT Solution</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt dolore magna aliqua
+          </p>
+          <a href="#">read more</a>
+        </div>
+
+        <div className="service">
+          <i class="flaticon-project-management"></i>
+          <h3>Web Development</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt dolore magna aliqua
+          </p>
+          <a href="#">read more</a>
+        </div>
+
+        <div className="service">
+          <i class="flaticon-project-management"></i>
+          <h3>Networking Services</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt dolore magna aliqua
+          </p>
+          <a href="#">read more</a>
+        </div>
+
+        <div className="service">
+          <i class="flaticon-project-management"></i>
+          <h3>SEO Optimization</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt dolore magna aliqua
+          </p>
+          <a href="#">read more</a>
+        </div>
+
+        <div className="service">
+          <i class="flaticon-project-management"></i>
+          <h3>App Optimization</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt dolore magna aliqua
+          </p>
+          <a href="#">read more</a>
+        </div>
+
+        <div className="service">
+          <i class="flaticon-project-management"></i>
+          <h3>Data Recovery</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt dolore magna aliqua
+          </p>
+          <a href="#">read more</a>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Services
